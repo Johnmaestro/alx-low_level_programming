f6337844a503dc550e3ba70c987dd559e8b1a659
Johnmaestro <johnmariita421@gmail.com>
2023-02-16 14:41:27 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,14 +4,14 @@
  * main - Entry point for the program
  *
  * This function prints a message to the console using
- * the puts function, and then returns 0 to indicate
+ * the printf function, and then returns 0 to indicate
  * that the program completed successfully.
  *
  * Return: 0 on success, nonzero value on error
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
