d8a0b09ea49fd2a5836da4dfdf857b28dfb7f6e0
Johnmaestro <johnmariita421@gmail.com>
2023-02-16 13:34:16 -0800

    hope it's the final commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,12 @@
 /*
  * main - Entry
  *
+ * This function prints out a text using the puts functiom
+ *
  * return: Always 0
  */
 int main(void)
 	{
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 	}
