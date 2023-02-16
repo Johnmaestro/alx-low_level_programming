d26d2c9d6700b0726d825c2b6a5dd746d181b7a2
Johnmaestro <johnmariita421@gmail.com>
2023-02-16 15:58:36 -0800

    write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ *main - Entry point of the program.
+ *
+ * return: returns 1 on succeeding
+ */
+int main(void)
+{
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, msg, 59);
+	return 1;
+}
