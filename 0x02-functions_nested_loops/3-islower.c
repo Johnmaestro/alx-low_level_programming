28b98509fbdb465a8baa055ef7d1957e00e86204
Johnmaestro <johnmariita421@gmail.com>
2023-02-21 11:16:30 -0800

    lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,11 @@
 #include "main.h"
-/*
+/**
+ * _islower - returns 1 if character is lowercase and 0
+ * if character is uppercase
  *
+ * @c: character's ascii code is passed to this parameter
+ *
+ * Return: returns 1 if lowercase and 0 if uppercase
  */
 int _islower(int c)
 {
