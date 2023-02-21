79943503ac5cd158cf911e2468817e0474bc93ae
Johnmaestro <johnmariita421@gmail.com>
2023-02-21 14:47:09 -0800

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - function that prints the time
+ *
+ */
+void jack_bauer(void)
+{
+	int i, j, m, n;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 3; j++)
+		{
+			for (m = 0; m <= 5; m++)
+			{
+				for (n = 0; n <= 9; n++)
+				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(':');
+					_putchar('0' + m);
+					_putchar('0' + n);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
