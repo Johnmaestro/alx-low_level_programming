cf1996c497175a4367ac42c7245d35320d476c4f
Johnmaestro <johnmariita421@gmail.com>
2023-02-21 08:30:01 -0800

    alpha x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * print_alphabet_x10 - prints a to z 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		char alpha;
+
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
