51db5ad60430da72fb7fb48e43a375a318096a5a
Johnmaestro <johnmariita421@gmail.com>
2023-02-22 12:29:58 -0800

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	int n;
+	int m = 0;
+
+	for (n = 0; n < 10; n++)
+	{
+		if ((n % 3 == 0) || (n % 5 == 0))
+		{
+			m = m + n;
+		}
+	}
+	printf("%d\n", m);
+	return (0);
+}
