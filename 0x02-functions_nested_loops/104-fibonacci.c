4de99aef817a0e35fdbbddfcfec78188d7200ab0
Johnmaestro <johnmariita421@gmail.com>
2023-02-22 14:06:51 -0800

    fib

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point for the program
+ *
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	long int i, c;
+	long int a = 1;
+	long int b = 2;
+
+	for (i = 1; i <= 98; i++)
+	{
+		if (i == 98)
+			printf("%ld\n", a);
+		else
+			printf("%ld, ", a);
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	return (0);
+}
