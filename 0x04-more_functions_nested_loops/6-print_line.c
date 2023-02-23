122c701e38c772b53b51fd2fb6bd65dd7720333e
Johnmaestro <johnmariita421@gmail.com>
2023-02-23 10:49:21 -0800

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - function to draw a line
+ *
+ * @n: number of lines
+ *
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	_putchar('\n');
+}
