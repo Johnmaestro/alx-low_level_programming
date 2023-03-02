7dd29f31bb0b0c0215c42a5ea3d8899943b9a581
Johnmaestro <johnmariita421@gmail.com>
2023-03-02 01:36:43 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		if (i >= n)
+			break;
+		dest[i] = src[i];
+		i++;
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
