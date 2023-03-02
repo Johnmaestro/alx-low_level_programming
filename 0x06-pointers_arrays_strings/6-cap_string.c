8816207368fb62a42245dedf69a3c9dd9d800baf
Johnmaestro <johnmariita421@gmail.com>
2023-03-02 20:06:05 +0300

    cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * cap_string - function to capitalizee string
+ * @c: pointer to dtring to be capitalized
+ * Return: returns capitalized string
+ *
+ */
+char *cap_string(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (((c[i] == ' ') || (c[i] == '.' || c[i] == '\n' || c[i] == '	' || c[i] == ',' || c[i] == ';' || c[i] == '!' || c[i] == '?' || c[i] == '"' || c[i] == 40 || c[i] == 41 || c[i] == 123 || c[i] == 125)) && (c[i + 1] >= 97 && c[i + 1] <= 122))
+			c[i + 1] -= 32;
+		i++;
+	}
+	return (c);
+}
