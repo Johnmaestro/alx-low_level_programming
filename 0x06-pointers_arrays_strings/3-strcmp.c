104df3f9aa30aaa2d1dc160f88d34f532100b4a1
Johnmaestro <johnmariita421@gmail.com>
2023-03-02 16:40:47 +0300

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j = 0;
+	int num = 15;
+	int m;
+
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != 0)
+		j++;
+
+	if (i > j)
+		num *= 1;
+	if (j > i)
+		num *= -1;
+	if (j == i)
+	{
+		while (s1[m] != '\0')
+		{
+			if (s1[m] > s2[m])
+			{
+				num *= 1;
+				break;
+			}
+			if (s1[m] < s2[m])
+			{
+				num *= -1;
+				break;
+			}
+			else 
+				num *= 0;
+			m++;
+		}
+	}
+	return (num);
+}
