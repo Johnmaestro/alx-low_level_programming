4637ea0282f6c1a888b8fb63a986ada95da0ca4b
Johnmaestro <johnmariita421@gmail.com>
2023-03-07 00:13:41 +0300

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - function
+ * @s: pointer to a pointer
+ * @to: character
+ *
+ *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,24 +1,28 @@
 #include "main.h"
 /**
- *
+ * _strspn - function declaration
+ * @s: string to be checked
+ * @accept: string being passed on
+ * Return: returns the size of a strings prefix
  *
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; s[i] != '\0'; i++) {
-	for (j = 0; accept[j] != '\0'; j++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == accept[j])
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			break;
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+	}
+		if (accept[j] == '\0')
+		{
+			return (i);
 		}
-        }
-	if (accept[j] == '\0')
-	{
-		return i;
 	}
-	}
-	return i;
+	return (i);
 }
