55f41492563b8c761f8107d125b1d7ef4ba23159
Johnmaestro <johnmariita421@gmail.com>
2023-03-11 23:10:59 +0300

    change

---
+++ 0x0A-argc_argv/change.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int x = atoi(argv[1]);
+	int change = 0;
+	int j;
+	int coins[] = {1, 2, 5, 10, 25};
+
+	for (j = 4; j >= 0 && x >= 0; j--)
+	{
+		while (x >= coins[j])
+		{
+			change++;
+			x -= coins[j];
+		}
+	}
+	printf("%d\n", change);
+	return (0);
+}
