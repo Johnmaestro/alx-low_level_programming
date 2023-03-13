3caaf7d82fdf96c119763b35add1812c194a7fc9
Johnmaestro <johnmariita421@gmail.com>
2023-03-13 20:34:23 +0300

    correction

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,9 +12,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *arr = (char *) malloc(size * sizeof(char));
 
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
 	if (size == 0)
 	{
-		return (NULL);;
+		return (arr);
 	}
 	for (; i < size; i++)
 	{
